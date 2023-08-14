962f513e73520a8987921c8733a52d97c2b4cf47
GTiko <gemechutiko@gmail.com>
2023-08-14 00:28:47 -0500

    updates

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    SERVER_URL: 'http://18.220.178.5:3000/',
+    // SERVER_URL: 'http://18.220.178.5:3000/',
+    SERVER_URL: 'https://book-store-6aqc.onrender.com/',
     KEY: 'BOOK_TOKEN',
 };
